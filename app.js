7899db4d13a6cfeecf9cc51e5e916066c47c615f
tlightk <klayxi@gmail.com>
2020-11-02 22:48:10 +0900

    installed mongodb and bodyparser

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, ".", "dist")));
+MongoClient.connect('mongodb-connection-string', (err, client) => {
+  app.use(express.static(path.resolve(__dirname, ".", "dist")));
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/api", (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+  app.post('/quotes', (req, res) => {
+    //
+  })
+
+  app.get("/api", (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
   });
 
-app.listen(9000, function () {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, ".", "dist", "index.html"));
+  });
+
+  app.listen(9000, function () {
     console.log('listening on port 9000');
+  })
+
+
 })
+
+
 
 module.exports = app;
