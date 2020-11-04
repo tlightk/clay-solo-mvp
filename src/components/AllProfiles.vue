9fbb23507260ff4027daee3fcf30790953f7c948
tlightk <klayxi@gmail.com>
2020-11-04 09:37:17 +0900

    fixed single and all profiles components

--- src/components/AllProfiles.vue
+++ src/components/AllProfiles.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="allProfiles">
-      <h1>This view is SingleProfile</h1>
+      <h1>This view is AllProfiles</h1>
       <input type="button" id="addProfileButton" v-on:click="$emit('add')"
       value="Add New Profile" />
   </div>
